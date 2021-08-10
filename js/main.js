eec2b562e3302f76f7e51b269eb0314b2797139a
IrynaHomoliako <88425695+IrynaHomoliako@users.noreply.github.com>
2021-08-10 08:10:28 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+
+
+$('.menu__btn').on('click', function(){
+  $('.menu').addClass('active');
+});
+$('.menu__btn--close').on('click', function(){
+    $('.menu').removeClass('active');
+  });
+
+
+
+});
+
+
